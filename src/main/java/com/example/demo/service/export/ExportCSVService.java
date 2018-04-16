9cdde0fb51434e65c7185676b01b66b1e3f22383
remifoyard <rfpro8@gmail.com>
2018-04-16 15:13:42 +0200

    CSV + XLSX OK

---
+++ src/main/java/com/example/demo/service/export/ExportCSVService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service.export;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.dto.ClientDTO;
+
+import java.io.IOException;
+import java.util.List;
+import java.io.Writer;
+import java.util.*;
+
+@Service
+public class ExportCSVService {
+
+    public void export(Writer printWriter, List<ClientDTO> clients) throws IOException {
+        //printWriter.write("HELLO WORLD RF !");
+        for(ClientDTO cl : clients){
+            printWriter.write(cl.getNom()+";"+cl.getPrenom()+";");
+        }
+    }
+}
